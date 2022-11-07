c0d237b6436f0a3cd446935e294af2766c011209
SanjithSenthil <sanjithsenthilr@gmail.com>
2022-11-07 14:41:18 -0800

    Added comment

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -1,47 +1,27 @@
 import pandas as pd
 import numpy as np
-import seaborn as sb
-
+import matplotlib.pyplot as plt
 
 def load_and_process(url_or_path_to_csv_file):
+    #Method Chain 1
+    df = (
+        pd.read_csv(url_or_path_to_csv_file)
+        .drop(["Price", "Category","Current Ver","Android Ver", "Last Updated"],axis=1)
+        .dropna()
+        .reset_index()
+        .rename(columns = {'Size':'Size MB','Reviews':'No. of reviews','Installs':'Downloads','Content Rating':'Audience'})
+        )
+    # We had asked the prof and he said we could do the below steps seperately.
+    df['Size MB'] = df['Size MB'].apply(lambda x: float(str(x).replace('k', '')) / 1000 if 'k' in str(x) else x)
+    df['Size MB'] = df['Size MB'].apply(lambda x: str(x).replace('Varies with device', 'NaN') if 'Varies with device' in str(x) else x)
+    df['Size MB'] = df['Size MB'].apply(lambda x: str(x).replace('M', '') if 'M' in str(x) else x)
+    df['Size MB'] = df['Size MB'].astype(float)
+    df['No. of reviews'] = df['No. of reviews'].astype(float)
+    df['Downloads'] = df['Downloads'].apply(lambda x: str(x).replace(',', '') if ',' in str(x) else x)
+    df['Downloads'] = df['Downloads'].apply(lambda x: str(x).replace('+', '') if '+' in str(x) else x)
+    df['Downloads'] = df['Downloads'].astype(float)
+    indexName = df[ (df['Rating'] < 1) & (df['Rating'] > 5) ].index
+    indexName = df[ (df['Rating'] < 1) & (df['Rating'] > 5) ].index
+    df.drop(indexName , inplace=True)
     
-    
-# We had asked the prof and he said we could use these steps because we could not convert
-#some steps properly
-
-#Method Chain 1
-df = (
-    pd.read_csv('../data/raw/google_play_store.csv')
-    .drop(["Price", "Category","Current Ver","Android Ver", "Last Updated"],axis=1)
-    .dropna()
-    .reset_index()
-)
-
-df['Size'] = df['Size'].apply(lambda x: float(str(x).replace('k', '')) / 1000 if 'k' in str(x) else x)
-df['Size'] = df['Size'].apply(lambda x: str(x).replace('Varies with device', 'NaN') if 'Varies with device' in str(x) else x)
-df['Size'] = df['Size'].apply(lambda x: str(x).replace('M', '') if 'M' in str(x) else x)
-df['Size'] = df['Size'].astype(float)
-df['Reviews'] = df['Reviews'].astype(float)
-
-#Method Chain 2
-df1 = (
-    df
-    .rename(columns = {'Size':'Size MB'})
-    .rename(columns = {'Reviews':'No. of reviews'})
-)
-
-df1['Installs'] = df1['Installs'].apply(lambda x: str(x).replace(',', '') if ',' in str(x) else x)
-df1['Installs'] = df1['Installs'].apply(lambda x: str(x).replace('+', '') if '+' in str(x) else x)
-df1['Installs'] = df1['Installs'].astype(float)
-indexName = df1[ (df1['Rating'] < 1) & (df1['Rating'] > 5) ].index
-
-#Method Chain 3
-df2 = (
-    df1
-)
-
-df2 = df2.rename(columns = {'Installs':'Downloads'})
-df2 = df2.drop(indexName)
-df2 = df2.rename(columns = {'Content Rating':'Audience'})
-df2 = df2.reset_index()
-df2.head()
+    return df
